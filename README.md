082638900ca0bd052434e83c348e549c8906a3b1
lasermtv07 <laserman.minecraft@seznam.cz>
2024-07-23 21:33:26 +0200

    added arithmetic operations and push instruction

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ still WIP, too.
 - `label` - defines a label to which you can jump
 - `let` - creates/sets the value of a variable, You can use `{1,2,3}`.. to define arrays,
 `&var` to create a reference, `*var` to set to the same value as different variable and you can ofcourse also create booleans, scalars and strings
+- `add/sub/mul/div/mod t a b` - do the corresponding arithmetic operations (`t` - target variable, `a` input 1, `b` input two, rules apply the same as for let)
+- `push` - pushes data onto stack, same rules as for let apply
--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+//TODO: fix broken strings (newline breaks them :/)
 function ir(code){
+	function parseArr(t){
+		let tt=t.split("");
+		tt.shift();
+		tt.pop();
+		tt=(tt.join("")).trim();
+		let type='num';
+		console.log(isNaN(tt.split(",")[0]))
+		if(tt.split(",")[0]=="true" || tt.split(",")[0]==false) type='bool';
+		else if(!isNaN(tt.split(",")[0])) type='num';
+		else if(tt[0]=="'" || t[0]=='"') type='str';
+		else throw new Error("[misaIR] Could not determine array type!");
+		tt=tt.split(",");
+		let o=[];
+		for(let j of tt){
+			j=j.trim();
+			switch(type){
+				case "bool":
+					if(j=="true") o.push(1);
+					else o.push(0);
+					break;
+				case "num":
+					o.push(parseInt(j));
+					break;
+				case "str":
+					j=j.split("");
+					j.shift();
+					j.pop();
+					o.push(j.join(""));
+			}
+		}
+		return {value:o,type:"arr<"+type+">"};
+	}
 	var l=code.split("\n");
 	var labels=[];
 	var stack=[];
@@ -52,42 +85,88 @@ function ir(code){
 
 					}
 					else if(t[0]=="{"){
-						let tt=t.split("");
-						tt.shift();
-						tt.pop();
-						tt=(tt.join("")).trim();
-						let type='num';
-						console.log(isNaN(tt.split(",")[0]))
-						if(tt.split(",")[0]=="true" || tt.split(",")[0]==false) type='bool';
-						else if(!isNaN(tt.split(",")[0])) type='num';
-						else if(tt[0]=="'" || t[0]=='"') type='str';
-						else throw new Error("[misaIR] Could not determine array type!");
-						console.log(type)
-						tt=tt.split(",");
-						let o=[];
-						for(let j of tt){
-							j=j.trim();
-							switch(type){
-								case "bool":
-									if(j=="true") o.push(1);
-									else o.push(0);
-									break;
-								case "num":
-									o.push(parseInt(j));
-									break;
-								case "str":
-									j=j.split("");
-									j.shift();
-									j.pop();
-									o.push(j.join(""));
-							}
-						}
-						vars[idx]={name:vars[idx].name,value:o,type:"arr<"+type+">"};
+						let arr=parseArr(t)
+						vars[idx]={name:vars[idx].name,value:arr.value,type:arr.type};
 					}
 				}
+				break;
+			case 'add':
+			case 'sub':
+			case 'mul':
+			case 'div':
+			case 'mod':
+				let targ=s[1];
+				let a=s[2];
+				let b=s[3];
+				let idx=-1;
+				for(let j=0;j<vars.length;j++){
+					if(vars[j].name==s[1]) idx=j;
+				}
+				if(idx==-1) throw new Error('[misaIR] Could not find variable');
+				if(s.length<4) throw new Error('[misaIR] Insufficent number of call parameters');
+				function parseVar(a){
+					if(a[0]=='*'){
+						a=a.split("");
+						a.shift();
+						a=a.join("");
+						for(let j of vars){
+							if(j.name==a && j.type=='num') a=parseInt(j.value);
+						}
+						if("*"+a==s[2]) throw new Error('[misaIR] Could not find variable');
+					}
+					else {
+						a=parseInt(a);
+						if(isNaN(a)) throw new Error('[misaIR] Error parsing integer');
+					}
+					return a;
+				};
+				a=parseVar(a);
+				b=parseVar(b);
+				if(s[0]=='add') vars[idx].value=a+b;
+				if(s[0]=='sub') vars[idx].value=a-b;
+				if(s[0]=='mul') vars[idx].value=a*b;
+				if(s[0]=='div') vars[idx].value=a/b;
+				if(s[0]=='mod') vars[idx].value=a%b;
+				break;
+			case 'push':
+				let p=s[1].trim();
+				if(p=='true') stack.push({value:1,type:'bool'});
+				else if(p=='false') stack.push({value:0,type:'bool'});
+				else if(!isNaN(parseInt(p))) stack.push({value:parseInt(p),type:'num'});
+				else if(p[0]=='"' || p[0]=="'"){
+					p=p.split("");
+					p.shift();
+					p.pop();
+					stack.push({value:p.join(""),type:'str'});
+				}
+				else if(p[0]=='{'){
+					let ar=parseArr(p);
+					stack.push({value:ar.value,type:ar.type});
+				}
+				else if(p[0]=='*'){
+					p=p.split("");
+					p.shift();
+					p=p.join("");
+					for(let j of vars){
+						if(j.name==p) stack.push(j);
+					}
+				}
+				else if(p[0]=='&'){
+					p=p.split("");
+					p.shift();
+					p=p.join("");
+					let exist=false;
+					for(let j of vars){
+						if(j.name==p) exist=true;
+					}
+					if(exist) stack.push({name:p,type:'ref'});
+					else throw new Error('[misaIR] Tried to reference nonexistent variable');
+				}
+				else throw new Error('[misaIR] Dont know what to push!');
 				break;
 		}
 	}
 	console.log(labels)
 	console.log(vars)
+	console.log(stack)
 }
